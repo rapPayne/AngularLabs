b94241ecd81d11c3caee55dc8ff66f8fb0d4a71b
rapPayne <bob@creator.net>
2017-08-25 15:39:49 -0500

    Added solutions

---
+++ wip/03_big_picture/src/app/receiving/receive-product.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'nw-receive-product',
+  templateUrl: './receive-product.component.html',
+  styleUrls: ['./receive-product.component.css']
+})
+export class ReceiveProductComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
