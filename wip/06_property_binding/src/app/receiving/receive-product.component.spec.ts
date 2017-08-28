5350e62768bdfe0682674a9d58338ea3cbedbc75
rapPayne <bob@creator.net>
2017-08-27 21:30:28 -0500

    Added solution for lab 6

---
+++ wip/06_property_binding/src/app/receiving/receive-product.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ReceiveProductComponent } from './receive-product.component';
+
+describe('ReceiveProductComponent', () => {
+  let component: ReceiveProductComponent;
+  let fixture: ComponentFixture<ReceiveProductComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ ReceiveProductComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ReceiveProductComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should be created', () => {
+    expect(component).toBeTruthy();
+  });
+});
