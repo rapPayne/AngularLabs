9c468cf1e3ada95ea3f53a1410c1672e6500a7ce
rapPayne <bob@creator.net>
2017-09-19 15:32:37 -0500

    Added solution for lab 10

---
+++ snapshots/10_forms_and_two_way_binding/src/app/app.module.ts
@@ -0,0 +1,32 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+
+import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { InventoryComponent } from './inventory/inventory.component';
+import { ReceiveProductComponent } from './receiving/receive-product.component';
+import { ShipOrderComponent } from './shipping/ship-order.component';
+import { OrdersToShipComponent } from './shipping/orders-to-ship.component';
+import { routing } from './app.router';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    DashboardComponent,
+    InventoryComponent,
+    ReceiveProductComponent,
+    ShipOrderComponent,
+    OrdersToShipComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    routing
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
