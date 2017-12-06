ab85773380c7fc698a407bf5b6ab27d965eb2b6d
rapPayne <bob@creator.net>
2017-12-06 08:27:00 -0600

    Updated settings and new lab

--- challenges/src/app/built-in-directives/built-in-directives.component.ts
+++ challenges/src/app/built-in-directives/built-in-directives.component.ts
@@ -3,7 +3,6 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'demo-built-in-directives',
   templateUrl: './built-in-directives.component.html',
-  styleUrls: ['./built-in-directives.component.css']
 })
 export class BuiltInDirectivesComponent implements OnInit {
   people;
