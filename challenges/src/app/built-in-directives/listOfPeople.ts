f50b3e461cafa60120df5de4f2c438efb5dcfdb5
rapPayne <bob@creator.net>
2018-01-17 14:38:31 -0600

    Moved WIP to a demo folder

--- challenges/src/app/built-in-directives/listOfPeople.ts
+++ challenges/src/app/built-in-directives/listOfPeople.ts
@@ -1,7 +1,7 @@
 export const people = [
-  { name: { first: "Samina", last: "Qazi" }, gender: "female", picture: { large: "https://media-exp2.licdn.com/media/AAEAAQAAAAAAAAM8AAAAJDMxNGQ0YTliLTk4NjItNDgxYS1iNTg0LWFkNjI0NWU4MDFhNw.jpg" } },
-  { name: { first: "Nikita", last: "Amaritya" }, gender: "female", picture: { large: "https://media-exp2.licdn.com/media/AAEAAQAAAAAAAAugAAAAJDcwNDFlOGE2LWM2NjQtNDBjMy04OTlmLWRjN2E2MDEzMDk4Mg.jpg" } },
-  { name: { first: "Charls", last: "Antony" }, gender: "male", picture: { large: "https://media-exp2.licdn.com/media/AAEAAQAAAAAAAANfAAAAJGZkMzliMGU2LTBlNjgtNDM2MC05YTY1LTViMjMwOTg4ZGI3Zg.jpg" } },
-  { name: { first: "Snehith", last: "Desu" }, gender: "male", picture: { large: "https://media-exp2.licdn.com/media/AAEAAQAAAAAAAAt1AAAAJDc5MjFkZDNjLWQ2ODMtNDg0YS1iODYyLTE1ZTMzNGFlMjFiOQ.jpg" } },
-  { name: { first: "Greg", last: "Harris" }, gender: "male", picture: { large: "https://media-exp2.licdn.com/media/p/6/000/235/0f3/16ad0d5.jpg" } },
+  { name: { first: "Janak", last: "Bhalla" }, gender: "male", picture: { large: "https://images-na.ssl-images-amazon.com/images/M/MV5BMTI4MzIxMTk0Nl5BMl5BanBnXkFtZTcwOTU5NjA0Mg@@._V1_SY1000_CR0,0,666,1000_AL_.jpg" } },
+  { name: { first: "Poorna", last: "Puranam" }, gender: "female", picture: { large: "https://images-na.ssl-images-amazon.com/images/M/MV5BMjIzOTA0OTQyN15BMl5BanBnXkFtZTcwMjE1OTIwMw@@._V1_.jpg" } },
+  { name: { first: "Anand", last: "Elayaperumal" }, gender: "male", location: {street: "2200 Augusta Blvd", city:"Bentonville", state: "AR", postcode: "72712" }, picture: { large: "https://images-na.ssl-images-amazon.com/images/M/MV5BMjA1MjE2MTQ2MV5BMl5BanBnXkFtZTcwMjE5MDY0Nw@@._V1_UX214_CR0,0,214,317_AL_.jpg" } },
+  { name: { first: "Christian", last: "Barrow" }, gender: "male", picture: { large: "https://images-na.ssl-images-amazon.com/images/M/MV5BMTM0NzYzNDgxMl5BMl5BanBnXkFtZTcwMDg2MTMyMw@@._V1_.jpg" } },
+  { name: { first: "Brenda", last: "Morgan" }, gender: "female", picture: { large: "https://images-na.ssl-images-amazon.com/images/M/MV5BMjEyMTEyOTQ0MV5BMl5BanBnXkFtZTcwNzU3NTMzNw@@._V1_SY1000_CR0,0,735,1000_AL_.jpg" } },
 ];
