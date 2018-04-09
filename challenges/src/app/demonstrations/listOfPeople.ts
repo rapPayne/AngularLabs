425e82a152ba199b7dd1ad53f059471cbb3ba1d0
rapPayne <bob@creator.net>
2018-04-09 09:40:13 -0500

    Updated challenges

---
+++ challenges/src/app/demonstrations/listOfPeople.ts
@@ -0,0 +1,7 @@
+export const people = [
+  { name: { first: "Dani", last: "Everett" }, gender: "female", picture: { large: "https://media.licdn.com/dms/image/C5603AQGr0g-Z70pfGg/profile-displayphoto-shrink_800_800/0?e=1527361200&v=alpha&t=z4-LNfc33wFjJp1h6RSYoW5fZznzWCPySoLs7gFyjEk" } },
+  { name: { first: "Kristine", last: "Aldridge" }, gender: "female", picture: { large: "" } },
+  { name: { first: "Venkat", last: "Pundru" }, gender: "male", picture: { large: "" } },
+  { name: { first: "Dennis", last: "Kalbarcyzk" }, gender: "male", location: { street: "84 Rainey Street", city: "Arlen", state: "TX", postcode: "75043" }, picture: { large: "" } },
+  { name: { first: "Shagufta", last: "Memon" }, gender: "female", picture: { large: "" } },
+];
