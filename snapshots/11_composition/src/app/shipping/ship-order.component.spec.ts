4ff9227bb01d98d9a17f6ce3dcaa4a65894343c4
rapPayne <bob@creator.net>
2018-05-25 16:49:58 -0500

    Updated solution to Angular 6

--- snapshots/11_composition/src/app/shipping/ship-order.component.spec.ts
+++ snapshots/11_composition/src/app/shipping/ship-order.component.spec.ts
@@ -19,7 +19,7 @@ describe('ShipOrderComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('should be created', () => {
     expect(component).toBeTruthy();
   });
 });
