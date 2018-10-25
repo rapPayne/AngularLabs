6c1fc7956cb9adda6e6a9998a9325164c8c3db7d
rapPayne <bob@creator.net>
2018-10-25 10:22:54 -0500

    Deleted old challenges

--- challenges/README.md
+++
@@ -1,3 +0,0 @@
-# How to run
-
-To run a particular solution, move it into demonstrations (for now). You may have to edit app.module.ts
--- challenges/src/app/solutions/lazyloading/lazy.routes.ts
+++
@@ -1,6 +0,0 @@
-import { RouterModule } from '@angular/router';
-import { LazyComponent } from './lazy.component';
-
-export const LazyRoutes = RouterModule.forChild([
-  {path:"", component:LazyComponent}
-]);
