d18d1be9dac878fe5732078253ca11d9898ac344
gespina <glenn.espina@gmail.com>
2020-05-04 17:52:42 -0700

    Added warehouse component

--- warehouse/src/app/app.module.ts
+++ warehouse/src/app/app.module.ts
@@ -2,14 +2,21 @@ import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 
 import { AppComponent } from "./app.component";
-import { DashboardComponent } from './dashboard/dashboard.component';
-import { InventoryComponent } from './inventory/inventory.component';
-import { ReceiveProductComponent } from './receiving/receive-product.component';
-import { ShipOrderComponent } from './shipping/ship-order.component';
-import { OrdersToShipComponent } from './shipping/orders-to-ship.component';
+import { DashboardComponent } from "./dashboard/dashboard.component";
+import { InventoryComponent } from "./inventory/inventory.component";
+import { ReceiveProductComponent } from "./receiving/receive-product.component";
+import { ShipOrderComponent } from "./shipping/ship-order.component";
+import { OrdersToShipComponent } from "./shipping/orders-to-ship.component";
 
 @NgModule({
-  declarations: [AppComponent, DashboardComponent, InventoryComponent, ReceiveProductComponent, ShipOrderComponent, OrdersToShipComponent],
+  declarations: [
+    AppComponent,
+    DashboardComponent,
+    InventoryComponent,
+    ReceiveProductComponent,
+    ShipOrderComponent,
+    OrdersToShipComponent,
+  ],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
