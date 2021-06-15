71df4f2212e1eb8dca9c5ed44b3ac9ee12476614
rapPayne <bob@creator.net>
2021-06-14 21:36:59 -0500

    Updated demo to NG12

--- demos/solutions/forms-status-tokens/src/app/app.module.ts
+++ demos/solutions/forms-status-tokens/src/app/app.module.ts
@@ -3,7 +3,6 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import { MaterialModule } from './material.module';
 
 import { AppComponent } from './app.component';
 import { FormWithStatusComponent } from './form-with-status/form-with-status.component';
@@ -16,7 +15,6 @@ import { FormWithStatusComponent } from './form-with-status/form-with-status.com
   imports: [
     BrowserModule,
     FormsModule,
-    MaterialModule,
     BrowserAnimationsModule,
   ],
   providers: [],
