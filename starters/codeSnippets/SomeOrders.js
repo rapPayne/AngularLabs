b0209cb60c236931727da0e1f2a8ed514fcaac39
rapPayne <bob@creator.net>
2022-12-27 18:21:48 -0600

    Simplified starters

--- starters/codeSnippets/SomeOrders.js
+++ starters/codeSnippets/SomeOrders.js
@@ -1,141 +1,144 @@
 this.orders = [
   {
-    "customerID": 1056,
-    "orderDate": new Date(),
-    "id": 10999,
-    "shipAddress": "Mehrheimerstr. 369",
-    "shipCity": "Köln",
-    "shipCountry": "Germany",
-    "shipName": "Ottilies Käseladen",
-    "shipPostalCode": "50739",
-    "shipRegion": "",
-    "shipVia": 2,
-    "tax": 0,
-    "shipping": 10,
-    "lines": [
+    customerID: 1056,
+    orderDate: new Date(),
+    id: 10999,
+    shipAddress: "Mehrheimerstr. 369",
+    shipCity: "Köln",
+    shipCountry: "Germany",
+    shipName: "Ottilies Käseladen",
+    shipPostalCode: "50739",
+    shipRegion: "",
+    shipVia: 2,
+    tax: 0,
+    shipping: 10,
+    lines: [
       {
-        "productID": 41,
-        "price": 9.65,
-        "quantity": 20
+        productID: 41,
+        price: 9.65,
+        quantity: 20
       },
       {
-        "productID": 51,
-        "price": 53,
-        "quantity": 15
+        productID: 51,
+        price: 53,
+        quantity: 15
       },
       {
-        "productID": 77,
-        "price": 13,
-        "quantity": 21
+        productID: 77,
+        price: 13,
+        quantity: 21
       }
     ],
-    "status": 1
+    status: 1,
+    customer: {},
   },
   {
-    "customerID": 1065,
-    "orderDate": new Date(),
-    "id": 11000,
-    "shipAddress": "2817 Milton Dr.",
-    "shipCity": "Albuquerque",
-    "shipCountry": "USA",
-    "shipName": "Rattlesnake Canyon Grocery",
-    "shipPostalCode": "87110",
-    "shipRegion": "NM",
-    "shipVia": 3,
-    "tax": 0,
-    "shipping": 10,
-    "lines": [
+    customerID: 1065,
+    orderDate: new Date(),
+    id: 11000,
+    shipAddress: "2817 Milton Dr.",
+    shipCity: "Albuquerque",
+    shipCountry: "USA",
+    shipName: "Rattlesnake Canyon Grocery",
+    shipPostalCode: "87110",
+    shipRegion: "NM",
+    shipVia: 3,
+    tax: 0,
+    shipping: 10,
+    lines: [
       {
-        "productID": 4,
-        "price": 22,
-        "quantity": 25
+        productID: 4,
+        price: 22,
+        quantity: 25
       },
       {
-        "productID": 24,
-        "price": 4.5,
-        "quantity": 30
+        productID: 24,
+        price: 4.5,
+        quantity: 30
       },
       {
-        "productID": 77,
-        "price": 13,
-        "quantity": 30
+        productID: 77,
+        price: 13,
+        quantity: 30
       }
     ],
-    "status": 1
+    status: 1,
+    customer: {},
   },
   {
-    "customerID": 1024,
-    "orderDate": new Date(),
-    "id": 11001,
-    "shipAddress": "Åkergatan 24",
-    "shipCity": "Bräcke",
-    "shipCountry": "Sweden",
-    "shipName": "Folk och fä HB",
-    "shipPostalCode": "S-844 67",
-    "shipRegion": "",
-    "shipVia": 2,
-    "tax": 0,
-    "shipping": 10,
-    "lines": [
+    customerID: 1024,
+    orderDate: new Date(),
+    id: 11001,
+    shipAddress: "Åkergatan 24",
+    shipCity: "Bräcke",
+    shipCountry: "Sweden",
+    shipName: "Folk och fä HB",
+    shipPostalCode: "S-844 67",
+    shipRegion: "",
+    shipVia: 2,
+    tax: 0,
+    shipping: 10,
+    lines: [
       {
-        "productID": 7,
-        "price": 30,
-        "quantity": 60
+        productID: 7,
+        price: 30,
+        quantity: 60
       },
       {
-        "productID": 22,
-        "price": 21,
-        "quantity": 25
+        productID: 22,
+        price: 21,
+        quantity: 25
       },
       {
-        "productID": 46,
-        "price": 12,
-        "quantity": 25
+        productID: 46,
+        price: 12,
+        quantity: 25
       },
       {
-        "productID": 55,
-        "price": 24,
-        "quantity": 6
+        productID: 55,
+        price: 24,
+        quantity: 6
       }
     ],
-    "status": 1
+    status: 1,
+    customer: {},
   },
   {
-    "customerID": 1071,
-    "orderDate": new Date(),
-    "id": 11002,
-    "shipAddress": "187 Suffolk Ln.",
-    "shipCity": "Boise",
-    "shipCountry": "USA",
-    "shipName": "Save-a-lot Markets",
-    "shipPostalCode": "83720",
-    "shipRegion": "ID",
-    "shipVia": 1,
-    "tax": 0,
-    "shipping": 10,
-    "lines": [
+    customerID: 1071,
+    orderDate: new Date(),
+    id: 11002,
+    shipAddress: "187 Suffolk Ln.",
+    shipCity: "Boise",
+    shipCountry: "USA",
+    shipName: "Save-a-lot Markets",
+    shipPostalCode: "83720",
+    shipRegion: "ID",
+    shipVia: 1,
+    tax: 0,
+    shipping: 10,
+    lines: [
       {
-        "productID": 13,
-        "price": 6,
-        "quantity": 56
+        productID: 13,
+        price: 6,
+        quantity: 56
       },
       {
-        "productID": 35,
-        "price": 18,
-        "quantity": 15
+        productID: 35,
+        price: 18,
+        quantity: 15
       },
       {
-        "productID": 42,
-        "price": 14,
-        "quantity": 24
+        productID: 42,
+        price: 14,
+        quantity: 24
       },
       {
-        "productID": 55,
-        "price": 24,
-        "quantity": 40
+        productID: 55,
+        price: 24,
+        quantity: 40
       }
     ],
-    "status": 1
+    status: 1,
+    customer: {},
   }
-
-]
+];
